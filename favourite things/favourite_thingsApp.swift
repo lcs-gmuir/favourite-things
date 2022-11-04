7153c0282344cb92463e6141490f5e6f86b26919
griffin muir <gmuir@lcs.on.ca>
2022-11-03 20:07:35 -0400

    Initial Commit

---
+++ favourite things/favourite_thingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  favourite_thingsApp.swift
+//  favourite things
+//
+//  Created by Samantha Stewart on 2022-11-03.
+//
+
+import SwiftUI
+
+@main
+struct favourite_thingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
